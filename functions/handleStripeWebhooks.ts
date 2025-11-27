0643a50383524627e9cd6a6e7465394b2cfc7239
harryexson <harryexson@hotmail.com>
2025-11-27 04:48:32 +0000

    File changes

--- functions/handleStripeWebhooks.ts
+++ functions/handleStripeWebhooks.ts
@@ -1,4 +1,4 @@
-import { createClientFromRequest } from 'npm:@base44/sdk@0.7.1';
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.4';
 import Stripe from 'npm:stripe@14.21.0';
 
 Deno.serve(async (req) => {
